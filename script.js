80e197b37934a3694f725ee55eca334f9ea2f429
Some Person <someperson>
2024-04-27 01:28:05 +0300

    Fixed Reconnection of Client when server reboots in WebSocket

--- script.js
+++ script.js
@@ -1,70 +1,72 @@
 //const io = require("socket.io-client");
 //const express = require('express');
-const { Telegraf} = require('telegraf')
-const WebSocket = require('ws');
-require('dotenv').config();
+const { Telegraf } = require("telegraf");
+const WebSocket = require("ws");
+require("dotenv").config();
 
 //const socket2 = io("wss://ws.postman-echo.com/socketio");
-
 
 const WEBSOCKETLINK = process.env.WEBSOCKETLINK;
 const TELEGRAMTOKEN = process.env.TELEGRAMTOKEN;
 const CHANNELID = process.env.CHANNELID;
 
+const bot = new Telegraf(TELEGRAMTOKEN, { handlerTimeout: 12000 });
+bot.start(async (ctx) => {});
 
-        const ws = new WebSocket(WEBSOCKETLINK, {
-            // perMessageDeflate: false
-        });
-        const bot = new Telegraf(TELEGRAMTOKEN, { handlerTimeout: 12000 })
-        bot.start(async (ctx) => {})
+async function connect() {
+  const ws = new WebSocket(WEBSOCKETLINK, {
+    // perMessageDeflate: false
+  });
 
-async function test() {
+  ws.on("connection", function connection() {
+    bot.telegram.sendMessage(CHANNELID, "Connection...");
+    console.log("connection");
+  });
 
-    ws.on('error', console.error);
+  ws.on("open", function open() {
+    bot.telegram.sendMessage(CHANNELID, "Connection successful! 🪀");
+    console.log("open");
+  });
 
-    ws.on("close", () => {
-        console.log("close");
-        bot.telegram.sendMessage(CHANNELID, "Connection closed :( smth went wrong  😭")
-    })
+  ws.on("error", console.error);
 
-    ws.on('connection', function open() {
-      console.log("connectiom")
-    });
-    
-    ws.on('message', function message(data) {
+  ws.on("close", () => {
+    console.log("close");
+    bot.telegram.sendMessage(
+      CHANNELID,
+      "Connection closed :( smth went wrong 😭 Re-connection..."
+    );
+    delete ws;
+    setTimeout(function () {
+      connect();
+    }, 15000);
+  });
 
-        if(data.toString().slice(0,1) === "0") {
-            console.log(data.toString().slice(0,1));
-            const res = JSON.stringify(40);
-            console.log(res);
-            ws.send(res);  
-        }
+  ws.on("message", function message(data) {
+    if (data.toString().slice(0, 1) === "0") {
+      console.log(data.toString().slice(0, 1));
+      const res = JSON.stringify(40);
+      console.log(res);
+      ws.send(res);
+    }
 
-        if(data.toString().slice(0,21) ===  '42["newPendingTicket"') {
-            bot.telegram.sendMessage(CHANNELID, "New ticket on VillageDAO 🚀")
-            console.log("NEW TICKET")
-        }
+    if (data.toString().slice(0, 21) === '42["newPendingTicket"') {
+      bot.telegram.sendMessage(CHANNELID, "New ticket on VillageDAO 🚀");
+      console.log("NEW TICKET");
+    }
 
-        if(data.toString() === "2") {
-            console.log(data);
-            const res = JSON.stringify(3);
-            console.log(res);
-            ws.send(res);  
-        }
+    if (data.toString() === "2") {
+      console.log(data);
+      const res = JSON.stringify(3);
+      console.log(res);
+      ws.send(res);
+    }
 
-        console.log('received: %s', data);
+    console.log("received: %s", data);
+  });
+}
 
-    });
-
- }
-
-
-test();
-
-
-
-
-
+connect();
 
 // 40
 
